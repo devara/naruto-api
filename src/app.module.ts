09d1b418e77f9dbb1ab4512eec71a189391c1e93
Katon Mahardika <devaraekokm@gmail.com>
2024-12-04 01:35:56 +0700

    feat(api): update app module an adapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AllConfigModule } from './config/config.module';
+import { DatabaseMongooseModule } from './database/db.mongoose.module';
+import { ApiModule } from './api/api.module';
+import { BaseModule } from './api/base/base.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AllConfigModule,
+    DatabaseMongooseModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 45,
+      },
+    ]),
+    BaseModule,
+    ApiModule,
+  ],
 })
 export class AppModule {}
