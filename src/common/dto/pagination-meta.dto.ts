05fe25fec88f2fc96a36b243039104efe19cee37
Katon Mahardika <devaraekokm@gmail.com>
2024-12-03 17:21:38 +0700

    feat(decorators): add some dto on project

---
+++ src/common/dto/pagination-meta.dto.ts
@@ -0,0 +1,46 @@
+import { Expose } from 'class-transformer';
+import { PaginationOptionDto } from './pagination-option.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PaginationMetaDto {
+  @Expose()
+  @ApiProperty()
+  readonly page: number;
+
+  @Expose()
+  @ApiProperty()
+  readonly per_page: number;
+
+  @Expose()
+  @ApiProperty()
+  readonly total?: number;
+
+  @Expose()
+  @ApiProperty()
+  readonly prev_page?: number;
+
+  @Expose()
+  @ApiProperty()
+  readonly next_page?: number;
+
+  @Expose()
+  @ApiProperty()
+  readonly total_pages: number;
+
+  constructor(totalRecords: number, pageOptions: PaginationOptionDto) {
+    this.page = pageOptions.page;
+    this.per_page = pageOptions.per_page;
+    this.total = totalRecords;
+
+    this.total_pages =
+      pageOptions.per_page > 0
+        ? Math.ceil(totalRecords / pageOptions.per_page)
+        : 0;
+
+    this.next_page = this.page < this.total_pages ? this.page + 1 : undefined;
+    this.prev_page =
+      this.page > 1 && this.page - 1 < this.total_pages
+        ? this.page - 1
+        : undefined;
+  }
+}
